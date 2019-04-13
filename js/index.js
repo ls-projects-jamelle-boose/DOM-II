a505de949e34fbde9616792feac86e400c25c241
user <19831942+Jamelle-Boose@users.noreply.github.com>
2019-04-13 13:15:28 -0700

    write mouseover event listner

--- js/index.js
+++ js/index.js
@@ -1,1 +1,19 @@
 // Your code goes here
+
+// Identify buttons requiring identical action
+const buttonList = document.querySelectorAll(".btn")
+
+// Click button event handler
+buttonList.forEach(button => button.addEventListener('click', function (event) {
+  alert(`Element clicked through function ${event.target}!`)
+}))
+
+// Identify anchor links requiring identical action
+const navList = document.querySelectorAll(".nav a")
+
+// Mouseover nav link event handler
+navList.forEach(link => link.addEventListener('mouseover', function (event) {
+  alert(`Element clicked through function ${event.target}!`), this
+}))
+
+
