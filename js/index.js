2edd675dd2f33f4e55ae3d3d4661f3789cd7cb9d
user <19831942+Jamelle-Boose@users.noreply.github.com>
2019-04-14 02:21:46 -0700

    complete wheel event handler

--- js/index.js
+++ js/index.js
@@ -8,9 +8,6 @@ buttonList.forEach(button => button.addEventListener('click', function (event) {
   alert(`Element clicked through function ${event.target}!`)
 }))
 
-
-
-
 // Identify anchor links requiring identical action
 const navList = document.querySelectorAll(".nav a")
 
@@ -19,7 +16,18 @@ navList.forEach(link => link.addEventListener('mouseover', function (event) {
   link.style.color = 'red'
 }))
 
+// Identify background
+const background = document.querySelector("body")
 
+//Keydown background even handler
+background.addEventListener('keydown', function (event) {
+  background.style.color = 'orange'
+})
 
+// Identify image in header
+const headIMG = document.querySelector('.home .intro img')
 
-
+// Wheel header image event handler
+headIMG.addEventListener('wheel', function (event) {
+  alert('Cheat code activated')
+})
